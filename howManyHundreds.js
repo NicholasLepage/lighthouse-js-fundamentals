b656c854aa403d8c0f9c05f18a1697ab15b6b692
Nicholas Lepage <nic.lep@hotmail.com>
2018-05-18 01:54:53 +0000

    Adds a few different exercises and also using this as an opportunity to see if I can write longer messages than simply 1-liners.

---
+++ howManyHundreds.js
@@ -0,0 +1,10 @@
+function howManyHundreds(num) {
+  var newNum = num - (num % 100);
+  return (newNum / 100);
+};
+
+console.log(howManyHundreds(1000), "=?", 10);
+console.log(howManyHundreds(894), "=?", 8);
+console.log(howManyHundreds(520), "=?", 5);
+console.log(howManyHundreds(99), "=?", 0);
+console.log(howManyHundreds(0), "=?", 0);
